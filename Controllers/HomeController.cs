5cc472620ffcdb4471b3dc3416abbe622ebf439d
Irwin-Vargas <irwin_vilca@usmp.pe>
2025-03-28 22:52:38 -0500

    commit 1 enviasrecibe

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,18 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Practica1.Models;
+using System.Text.Json;
+
 
 namespace Practica1.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly HttpClient _httpClient;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController()
     {
-        _logger = logger;
+        _httpClient = new HttpClient();
     }
 
     public IActionResult Index()
@@ -18,14 +20,38 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult EnviasRecibe()
     {
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [HttpPost]
+    public IActionResult ProcesarCambio(string monedaOrigen, string monedaDestino, decimal cantidad)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        decimal tipoCambio = 1.0m; // Valor por defecto
+
+        // Definir el tipo de cambio fijo
+        if (monedaOrigen == "BRL" && monedaDestino == "PEN")
+        {
+            tipoCambio = 0.634m; // 1 BRL = 0.634 PEN
+        }
+        else if (monedaOrigen == "PEN" && monedaDestino == "BRL")
+        {
+            tipoCambio = 1 / 0.634m; // 1 PEN = 1.577 BRL (inverso)
+        }
+
+        decimal montoConvertido = cantidad * tipoCambio;
+
+        TempData["MonedaOrigen"] = monedaOrigen;
+        TempData["MonedaDestino"] = monedaDestino;
+        TempData["Cantidad"] = cantidad;
+        TempData["MontoConvertido"] = montoConvertido;
+
+        return RedirectToAction("Boleta");
+    }
+
+    public IActionResult Boleta()
+    {
+        return View();
     }
 }
