9cde2e9622ad065d3f217e8842f3cd304fe6bbc3
Irwin-Vargas <irwin_vilca@usmp.pe>
2025-03-28 23:08:45 -0500

    commit 3 boleta

--- obj/Debug/net9.0/Practica1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Practica1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practica1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+298ec42bfde37917b44b60bab0a02b5ddb5690a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99c250332fcb06320e26a14697b6b40c025808e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practica1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practica1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
